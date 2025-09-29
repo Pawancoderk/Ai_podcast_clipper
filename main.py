d367b59b4201928fa271b991a9845ceb6d3d7660
Pawan <imrealpawan@gmail.com>
2025-09-29 20:28:19 +0530

    feat: improve vertical video creation with enhanced frame handling and subtitle generation

--- main.py
+++ main.py
@@ -48,26 +48,27 @@ mount_path = "/root/.cache/torch"
 
 auth_scheme = HTTPBearer()
 
+
 def create_vertical_video(tracks, scores, pyframes_path, pyavi_path, audio_path, output_path, framerate=25):
     target_width = 1080
     target_height = 1920
 
-    flist = glob.glob(os.path.join(pyframes_path,"*jpg"))
+    flist = glob.glob(os.path.join(pyframes_path, "*jpg"))
     flist.sort()
 
     faces = [[] for _ in range(len(flist))]
 
     for tidx, track in enumerate(tracks):
         score_array = scores[tidx]
-        for fidx, frame in enumerate(track[["track"] ["frame"].tolist()]):
+        for fidx, frame in enumerate(track[["track"]["frame"].tolist()]):
             slice_start = max(fidx - 30, 0)
             slice_end = min(fidx + 30, len(score_array))
             score_slice = score_array[slice_start:slice_end]
             avg_score = float(np.mean(score_slice))
 
             faces[frame].append(
-                 {'track': tidx, 'score': avg_score, 's': track['proc_track']["s"][fidx], 'x': track['proc_track']["x"][fidx], 'y': track['proc_track']["y"][fidx]})
-    
+                {'track': tidx, 'score': avg_score, 's': track['proc_track']["s"][fidx], 'x': track['proc_track']["x"][fidx], 'y': track['proc_track']["y"][fidx]})
+
     temp_video_path = os.path.join(pyavi_path, "video_only.mp4")
 
     vout = None
@@ -80,27 +81,28 @@ def create_vertical_video(tracks, scores, pyframes_path, pyavi_path, audio_path,
 
         max_score_face = max(
             current_faces, key=lambda face: face['score']) if current_faces else None
-        
+
         if max_score_face and max_score_face['score'] < 0:
             max_score_face = None
 
         if vout is None:
             vout = ffmpegcv.VideoWriter(
-                file = temp_video_path,
+                file=temp_video_path,
                 codec=None,
-                fps = framerate,
+                fps=framerate,
                 resize=(target_width, target_height)
             )
-        
+
         if max_score_face:
             mode = "crop"
         else:
             mode = "resize"
-        
+
         if mode == "resize":
             scale = target_width / img.shape[1]
             resized_height = int(img.shape[0] * scale)
-            resized_image = cv2.resize(img, (target_width, resized_height), interpolation=cv2.INTER_AREA)
+            resized_image = cv2.resize(
+                img, (target_width, resized_height), interpolation=cv2.INTER_AREA)
 
             scale_for_bg = max(
                 target_width / img.shape[1], target_height / img.shape[0])
@@ -108,30 +110,80 @@ def create_vertical_video(tracks, scores, pyframes_path, pyavi_path, audio_path,
             bg_height = int(img.shape[0] * scale_for_bg)
 
             blurred_background = cv2.resize(img, (bg_width, bg_height))
-            blurred_background = cv2.GaussianBlur(blurred_background, (121, 121), 0)
+            blurred_background = cv2.GaussianBlur(
+                blurred_background, (121, 121), 0)
 
             crop_x = (bg_width - target_width) // 2
             crop_y = (bg_height - target_height) // 2
-            blurred_background = blurred_background[crop_y: crop_y + target_height, crop_x: crop_x + target_width]
+            blurred_background = blurred_background[crop_y: crop_y +
+                                                    target_height, crop_x: crop_x + target_width]
 
             center_y = {target_height - resized_height} // 2
-            blurred_background[center_y: center_y + resized_height, :] = resized_image
+            blurred_background[center_y: center_y +
+                               resized_height, :] = resized_image
 
             vout.write(blurred_background)
 
         elif mode == "crop":
             scale = target_height / img.shape[0]
-            resized_image = cv2.resize(img, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
+            resized_image = cv2.resize(
+                img, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
             frame_width = resized_image.shape[1]
-            center_x = int(max_score_face["x"] * scale  if max_score_face else frame_width // 2)
-            top_x = max(min(center_x - target_width // 2, frame_width - target_width), 0)
+            center_x = int(
+                max_score_face["x"] * scale if max_score_face else frame_width // 2)
+            top_x = max(min(center_x - target_width // 2,
+                        frame_width - target_width), 0)
 
-            image_cropped = resized_image[0: target_height, top_x: top_x + target_width]
+            image_cropped = resized_image[0: target_height,
+                                          top_x: top_x + target_width]
+
+            vout.write(image_cropped)
+    if vout:
+        vout.release()
+
+    ffmpeg_command = (f"ffmpeg -y -i {temp_video_path} -i {audio_path}"
+                      f" -c:v h264 -preset fast -crf 23 -c:a aac -b:a 128k"
+                      f" {output_path}")
+    subprocess.run(ffmpeg_command, shell=True, check=True, text=True)
 
 
+def create_subtitles_with_ffmpeg(transcript_segments: list, clip_start: float, clip_end: float, clip_video_path: str, output_path: str, max_word: int = 5):
+    temp_dir = os.path.dirname(output_path)
+    subtitle_path = os.path.join(temp_dir, "temp_subtitles.ass")
+
+    clip_segments = [segment for segment in transcript_segments 
+                     if segment.get("start") is not None
+                     and segment.get("end") is not None
+                     and segment.get("end") > clip_start
+                     and segment.get("start") > clip_start]
+    
+    subtitles = []
+    current_words = []
+    current_start = None
+    current_end = None
+
+    for segment in clip_segments:
+        word = segment.get("word", "").strip()
+        seg_start = segment.get("start")
+        seg_end = segment.get("end")
+
+        if not word or seg_start is None or seg_end is None:
+            continue
+
+        start_rel = max(0.0, seg_start - clip_start)
+        end_rel = max(0.0, seg_start - clip_start)
+
+        if end_rel <= 0:
+            continue
+
+        if not current_words:
+            current_start = start_rel
+            current_end = end_rel
+            
+
+# creates a single short clip
 
 
-# creates a single short clip  
 def process_clip(base_dir: str, original_video_path: str, s3_key: str, start_time: float, end_time: str, clip_index: int, transceipt_segments: list):
     clip_name = f"clip{clip_index}"
     s3_key_dir = os.path.dirname(s3_key)
@@ -157,37 +209,46 @@ def process_clip(base_dir: str, original_video_path: str, s3_key: str, start_tim
     duration = end_time - start_time
     cut_command = f"ffmpeg -i {original_video_path} -ss {start_time} -t {duration} {clip_segment_path}"
 
-    subprocess.run(cut_command, shell=True, check=True, capture_output=True, text=True)
+    subprocess.run(cut_command, shell=True, check=True,
+                   capture_output=True, text=True)
 
     extract_cmd = f"ffmpeg -i {clip_segment_path} -vn -acodec pcm_s16le -ac 1 {audio_path}"
     subprocess.run(extract_cmd, shell=True, check=True, capture_output=True)
 
     shutil.copy(clip_segment_path, base_dir / f"{clip_name}.mp4")
 
-    columbia_command = (f"python Columbia_test.py --videoName {clip_name}"
-                        f"--videoFolder {str(base_dir)}"
+    columbia_command = (f"python Columbia_test.py --videoName {clip_name} "
+                        f"--videoFolder {str(base_dir)} "
                         f"--pretrainModel weight/finetuning_TalkSet.model")
-    
+
     columbia_start_time = time.time()
-    subprocess.run(columbia_command,cwd="/asd" ,shell=True )
+    subprocess.run(columbia_command, cwd="/asd", shell=True)
     columbia_end_time = time.time()
-    print(f"Columbia script completed in {columbia_end_time - columbia_start_time:.2f} seconds")
+    print(
+        f"Columbia script completed in {columbia_end_time - columbia_start_time:.2f} seconds")
 
     tracks_path = clip_dir / "pywork" / "tracks.pckl"
     scores_path = clip_dir / "pywork" / "scores.pckl"
     if not tracks_path.exists() or not scores_path.exists():
         raise FileNotFoundError("Tracks or scores are not found for clips")
-    
+
     with open(tracks_path, "rb") as f:
         tracks = pickle.load(f)
-    
+
     with open(scores_path, "rb") as f:
         scores = pickle.load(f)
 
     cvv_start_time = time.time()
-    create_vertical_video(tracks, scores, pyavi_path, pyavi_path, audio_path, vertical_mp4_path)
+    create_vertical_video(tracks, scores, pyavi_path,
+                          pyavi_path, audio_path, vertical_mp4_path)
     cvv_end_time = time.time()
-    print(f"Clip {clip_index} vertical video vreation time: {cvv_end_time - cvv_start_time:.2f} seconds")
+    print(
+        f"Clip {clip_index} vertical video vreation time: {cvv_end_time - cvv_start_time:.2f} seconds")
+
+    s3_client = boto3.client("s3")
+    s3_client.upload_file(
+        vertical_mp4_path, "ai-podcast-clipper", output_s3_key)
+
 
 @app.cls(gpu="L40S", timeout=900, retries=0, scaledown_window=20, secrets=[modal.Secret.from_name("ai-podcast-clipper-secret")], volumes={mount_path: volume})
 class AiPodcastClipper:
@@ -295,31 +356,31 @@ This is a podcast video transcript consisting of word, along with each words's s
         # 2 Identify moments for clips with Gemini
         print("Identifying the clip moments...")
         identified_moments_raw = self.identify_moments(transcript_segments)
-        
+
         cleaned_json_string = identified_moments_raw.strip()
         if cleaned_json_string.startswith("'''json"):
             cleaned_json_string = cleaned_json_string[len("'''json"):].strip()
         if cleaned_json_string.endswith("'''"):
             cleaned_json_string = cleaned_json_string[:-len("'''")].strip()
-        
+
         clip_moments = json.loads(cleaned_json_string)
         if not clip_moments or not isinstance(clip_moments, list):
             print("Error: Identified moments is not a list")
             clip_moments = []
-        
+
         print(clip_moments)
 
         # 3 Process clips
         for index, moment in enumerate(clip_moments):
-            if "start" in moment  and "end" in moment:
-                print(f"Processing clip {index} from {moment['start']} to {moment['end']}")
-                process_clip(base_dir, video_path, s3_key, moment["start"], moment["end"], index, transcript_segments)
-        
+            if "start" in moment and "end" in moment:
+                print(
+                    f"Processing clip {index} from {moment['start']} to {moment['end']}")
+                process_clip(base_dir, video_path, s3_key,
+                             moment["start"], moment["end"], index, transcript_segments)
+
         if base_dir.exists():
-            print("Cleaning up temp dict after" + base_dir)
+            print(f"Cleaning up temp dir after {base_dir}")
             shutil.rmtree(base_dir, ignore_errors=True)
-
-
 
         # print(os.listdir(base_dir))
 
